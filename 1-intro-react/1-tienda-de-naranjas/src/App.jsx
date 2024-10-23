866ee63ad354a29db67230367692876751bcf990
andresmonterroza <monterrosamercadoandres@gmail.com>
2024-10-23 07:34:20 -0500

    creacio de tiendas react

---
+++ 1-intro-react/1-tienda-de-naranjas/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return(
+    <div>
+      <header><nav>menu de navegacio</nav>
+      </header>
+      <section>seccion promos 🍊</section>
+      <section>seccion tienda 🛒</section>
+    </div>
+  )
+}
+
+export default App
