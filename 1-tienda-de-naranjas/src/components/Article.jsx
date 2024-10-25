1bb39b2665da59f16d02af0bb4715ed442ac07a3
andresmonterroza <monterrosamercadoandres@gmail.com>
2024-10-25 07:32:21 -0500

    creacio de cards

---
+++ 1-tienda-de-naranjas/src/components/Article.jsx
@@ -0,0 +1,7 @@
+
+
+export default function Article() {
+  return (
+    <article>seccion tienda 🛒</article>
+  )
+}
---
+++ 1-tienda-de-naranjas/src/components/Header.jsx
@@ -0,0 +1,7 @@
+ export function Header (){
+
+return ( <header><nav>menu de navegacio</nav>
+      </header>)
+
+}
+
